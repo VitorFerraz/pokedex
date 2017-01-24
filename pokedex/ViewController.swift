d06eb472e44e7af5abde658d3880cc6379ac3d7f
Vitor Ferraz <vitorfvarela@gmail.com>
2017-01-24 20:50:36 -0200

    implementado protocolos: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collection:UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collection.delegate = self
+        collection.dataSource = self
+//        let charmander = Pokemon(name: "Charmander", pokedexId: 4)
+//        print(charmander.name)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //onde criamos as cells
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell{
+            let pokemon = Pokemon(name: "Pokemon", pokedexId: indexPath.row)
+            cell.configureCell(pokemon: pokemon) //chama o metodo no pokecell para setar o lbl e img
+            return cell
+        }else{
+            return UICollectionViewCell()
+        }
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 400 // retorna quantos objetos na collection
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 108, height: 108)
+        
+    }
+    
+    
 }
 
